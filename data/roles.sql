545331ceca6d697e9f81b4828da3d3e3190d1e4c
xavierparedesaguilar <paredesaguilarxavier@gmail.com>
2023-08-18 17:01:41 -0500

    add roles.sql for user

---
+++ data/roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user` (`id`, `role_id`, `username`, `names`, `surnames`, `email`, `password`, `token`, `gender`, `status`, `created`, `modified`, `station_id`) VALUES
+(35, 1, 'test.test', 'test', 'test', 'TEST@GMAIL.COM', 'dc0793a027ad0dfdf3a6b42df14cf2b0', '97be0320b09d97031a67feca00be3197', '1', 1, '2023-07-15 15:26:36', '2023-07-15 15:26:36', 8);
+insert into module VALUES(49,'42',1,'Zona de Agrobiodiversidad','zabd','fa fa-eyedropper','Zonas de Agrobiodiversidad ZABD',1,'','',2,'Zabd');
+insert into module_user VALUES(2731,49,16,'1,2,3,4,5,6,7',1,'','');
